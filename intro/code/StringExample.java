70d6aafa0d10e26b8bbdc97d7b2fa01ed8fc34d3
Andrew Rosen <andrew.rosen@temple.edu>
2017-09-13 19:57:56 -0400

    New intro code

---
+++ intro/code/StringExample.java
@@ -0,0 +1,15 @@
+
+public class StringExample {
+
+	public static void main(String[] args) {
+		int x;
+		double d;
+
+		for(int i = 0; i < 256; i++) {
+			System.out.println(i + "\t" + (char) i); 
+		}
+
+
+	}
+
+}
