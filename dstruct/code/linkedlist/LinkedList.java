62416bef0236960fc733a34a80393f96b080904c
Andrew Rosen <rosen@temple.edu>
2017-09-18 13:54:01 -0400

    Changes

---
+++ dstruct/code/linkedlist/LinkedList.java
@@ -0,0 +1,88 @@
+package linkedlist;
+
+public class LinkedList<E> {
+	private int size;
+	private Node<E> head;
+	
+	public LinkedList() {
+		size = 0;
+		head = null;
+	}
+	
+	
+	
+	public boolean add(E item) {
+		add(size, item);
+		return true;
+	}
+
+
+
+	public void add(int index, E item) {
+		//index out of bounds 
+		if(index < 0 || index > size) {
+			throw new ArrayIndexOutOfBoundsException(index);
+		}
+		
+		Node<E> temp = new Node<E>(item);
+		
+		// adding a new head ( index 0)
+		if(index == 0) {
+			temp.next = head;
+			head = temp;	
+		} 
+		// adding anywhere else
+		else { 
+			Node<E> node = getNode(index - 1);
+			temp.next = node.next;
+			node.next = temp;
+		}
+		
+		
+		size++;
+	}
+	
+	private Node<E> getNode(int index){
+		if(index < 0 || index >= size) {
+			throw new ArrayIndexOutOfBoundsException(index);
+		}
+		
+		Node<E> current = head;
+		for(int i = 0 ; i < index; i++) {
+			current = current.next;
+		}
+		return current;
+		
+	}
+	
+	
+	public String toString() {
+		String  retval = "[ ";
+		Node<E> current = head;
+		
+		while(current != null) {
+			retval =  retval + current.item.toString() +" ";
+			current = current.next;
+		}
+		
+		
+		return retval + "]";
+	}
+	
+	public static void main(String[] args) {
+		LinkedList<String> list = new LinkedList<String>();
+		list.add(0, "Dick");
+		list.add(0, "Tom");
+
+		list.add(0, "Ann");
+		list.add(2, "Joe");
+		list.add("Andrew");
+		System.out.println(list);
+		System.out.println(list.getNode(0).item);
+		System.out.println(list.getNode(1).item);
+		System.out.println(list.getNode(2).item);
+		//System.out.println(list.getNode(3).item);
+	}
+	
+	
+}
--- dstruct/code/linkedlist/Node.java
+++ dstruct/code/linkedlist/Node.java
@@ -1,14 +1,12 @@
-package eveningReview.LinkedList;
+package linkedlist;
 
 public class Node<E> {
 	E item;
 	Node<E> next;
 	
-	
 	public Node(E item) {
 		this.item = item;
-		this.next = null;
-		
+		next = null;
 	}
-	
+
 }
