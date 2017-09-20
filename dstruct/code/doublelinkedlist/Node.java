6d826be1ea02fa6dac58eebb5fe9b9b44b0c3717
Andrew Rosen <andrew.rosen@temple.edu>
2017-09-20 13:56:51 -0400

    All but the remove() method done

---
+++ dstruct/code/doublelinkedlist/Node.java
@@ -0,0 +1,13 @@
+package doublelinkedlist;
+
+public class Node<E> {
+	E item;
+	Node<E> next;
+	Node<E> prev;
+	
+	public Node(E item) {
+		this.item = item;
+		this.next = null;
+		this.prev = null;
+	}
+}
