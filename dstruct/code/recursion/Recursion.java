711cf13ac11a59ae72c6eeea267b6333949f5225
Andrew Rosen <andrew.rosen@temple.edu>
2017-10-04 13:54:37 -0400

    TOday's code

--- dstruct/code/recursion/Recursion.java
+++ dstruct/code/recursion/Recursion.java
@@ -14,7 +14,7 @@ public class Recursion {
 	}
 	
 	public static long factorial(int n) {
-		if(n == 0 || n == 1) {
+		if(n == 0 ) {
 			return 1;
 		} else {
 			return n * factorial(n - 1);
@@ -22,9 +22,63 @@ public class Recursion {
 		
 	}
 	
+	
+	public static int length(String s) {
+		if(s.equals("") ) {
+			return 0 ;
+		} else {
+			return 1 + length(s.substring(1));
+		}
+		
+	}
+	
+	public static void print(String s) {
+		if(s.equals("") ) {
+			System.out.println();
+		} else {
+			print(s.substring(1));
+			System.out.print(s.charAt(0));  
+		}
+		
+	}
+	
+	public static long fib(int n) {
+		long[] memo = new long[n+1];
+		return fib(n, memo);
+	}
+	
+	
+	
+	private static long fib(int n, long[] memo) {
+		if(n == 1 ||  n == 2) {
+			return 1;
+		} else {
+			if(memo[n] != 0) {
+				return memo[n];
+			}
+			memo[n] =  fib(n-1,memo) + fib(n-2, memo);
+			return memo[n];
+		}
+	}
+	
+	
+	public static double pow(int base, int exp) {
+		if(exp == 0) {
+			return 1;
+		}else if(exp < 0) {
+			return 1.0/ pow(base,-1*exp);
+		}
+		else {
+			return base * pow(base, exp - 1);
+		}
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(factorial(10));
+		//print("alice");
+		System.out.println(pow(2,-8));
+		
 	}
 
 }
