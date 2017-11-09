3c606fff1f1883a96deb210d8b6d70c2db5fe09f
Andrew Rosen <andrew.rosen@temple.edu>
2017-11-08 19:21:33 -0500

    Class code

--- dstruct/code/HashTable/HashTable.java
+++ dstruct/code/HashTable/HashTable.java
@@ -1,14 +1,15 @@
 package HashTable;
 
 public class HashTable<K, V> {
-	
+	public final static int INITIAL_SIZE= 5;
+	public final static double LOAD_FACTOR = 0.5;
 	private int size;
 	private Entry<K,V>[] table; 
 	
 	
 	public HashTable() {
 		size = 0; 
-		this.table =  new Entry[5];
+		this.table =  new Entry[INITIAL_SIZE];
 	}
 
 	
@@ -17,24 +18,19 @@ public class HashTable<K, V> {
 		int index = key.hashCode() % table.length; 
 		
 		while(table[index] != null ) {
-			
-			
-			if(table[index].key.equals(key)) {
+
+			if(key.equals(table[index].key)) {
 				break;
 			}
-			
+			System.out.println("Collision for key " + key);
 			index= index + 1 % table.length;
 			
 		}
 		table[index] = new Entry<K,V>(key, value);
 		
-		
-		
-		
-		
 		size++;
 		// if hash table is too big, rehash()
-		if(1.0 * size / table.length >  0.5) {
+		if(1.0 * size / table.length >  LOAD_FACTOR) {
 			rehash();
 		}
 		
@@ -43,10 +39,7 @@ public class HashTable<K, V> {
 	public V get(K key) {
 		int index =  key.hashCode() % table.length;
 		while(table[index] != null) {
-			if(table[index].key == null) {
-				index = index + 1  %  table.length;
-			}
-			else if(table[index].key.equals(key)) {
+			if(key.equals(table[index].key)) {
 				return table[index].value;
 			} else {
 				index = index + 1  %  table.length;
@@ -62,32 +55,64 @@ public class HashTable<K, V> {
 	private void rehash() {
 		Entry<K,V>[] oldTable = table;
 		size = 0;
-		table =  new Entry[table.length * 2 +1];
+		table =  new Entry[table.length * 2 + 1];
 		for(int i = 0; i< oldTable.length; i++) {
-			if(oldTable[i] == null) {
-				continue;
-			} else if(oldTable[i].key == null) { 
+			if(oldTable[i] == null  || oldTable[i].key == null) {
 				continue;
 			} else {
 				Entry<K,V> entry =  oldTable[i]; 
-				int index = entry.key.hashCode()  % table.length;
-				table[index] =  entry;
-				size++;
+				this.put(entry.key, entry.value);
 			}
 			
 		}
 		
 	}
 	
+	public V delete( K key) {
+		V retval  = null;
+		int index =  key.hashCode() % table.length;
+		
+		while(table[index] != null) {
+			if(table[index].key.equals(key)) {
+				retval = table[index].value;
+				table[index].key = null;
+				table[index].value = null;
+				return retval;
+			} else {
+				index = index + 1  %  table.length;
+			}
+			
+		}
+		
+		return retval;
+		
+		
+		
+	}
+	
+	
 	
 	public static void main(String[] args) {
 		
 		HashTable<Integer, String> h =  new HashTable<Integer, String>();
-		h.put(1, "hello");
-		h.put(6, "world");
-		System.out.println(h.get(1));
-
-		System.out.println(h.get(2));
+		h.put(1, "1");
+		h.put(6, "6");
+		
+		
+		for(int i =1000; i>=0; i--) {
+			h.put(i, i+"");
+		}
+		
+		for(int i =0; i<500; i++) {
+			h.delete(i);
+		}
+		
+		for(int i = 0; i <= 1000; i++) {
+			System.out.println(h.get(i));
+		}
+		
+		
+		
 	}
 	
 	
