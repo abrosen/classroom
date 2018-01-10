e64b820eb9b0e6a1cb757ed131fdcbea75bcb4f9
Andrew Rosen <profandrewrosen@gmail.com>
2018-01-10 14:50:47 -0500

    Switching out computer

--- intro/code/Hello.java
+++ intro/code/Hello.java
@@ -1,6 +1,13 @@
 
 public class Hello {
+	
+	public static int add(int a , int b) {
+		return a +b;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
+		System.out.println("Foobar.");
+		System.out.println(add(5,2));
 	}
 }
