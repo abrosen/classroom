654ca0586cf48ab2cbafd993b95f7374300cfaba
Andrew Rosen <andrew.rosen@temple.edu>
2018-02-14 17:46:16 -0500

    Done

--- intro/code/Hello.java
+++ intro/code/Hello.java
@@ -1,12 +1,26 @@
 
 public class Hello {  // creates a program called Hello, stored in Hello.java
 	
+	
+	
+	/*public static int eul1() {
+		int sum = 0;
+		for(int i = 1; i< 1000; i++) {
+			if(i % 3 ==0 || i % 5 ==0) {
+
+				sum += i;
+			}
+		}
+		return sum;
+	}*/
+	
 	public static void main(String[] args) {  // The main method
-		
+	
+	
 		// Timmy went out of his house and went to the 
 	    // the store for bread.
 		System.out.println("Juliet: \n\t\"Romeo, Romeo, wherefore art thou Romeo\"?");
 		
-		System.out.println("\\\n\\");  // a print statement
+		//System.out.println(eul1());  // a print statement
 	}
 }
