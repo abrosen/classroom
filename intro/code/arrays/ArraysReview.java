8e606183c5741379c0820194ea83a74ec53743bd
Andrew Rosen <andrew.rosen@temple.edu>
2018-03-08 10:31:21 -0500

    Checking this commit manager

---
+++ intro/code/arrays/ArraysReview.java
@@ -0,0 +1,7 @@
+package arrays;
+
+        public class ArraysReview {
+            public static void main(String[] args) {
+                System.out.println("Hellokjdfhadskjhaskd I promise I can  actually spell!");
+            }
+}
