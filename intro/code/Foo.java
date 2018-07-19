04159211760d7906031e39436bf0d70ab97db4bd
Andrew Rosen <profandrewrosen@gmail.com>
2018-07-19 17:52:16 +0900

    Foo

---
+++ intro/code/Foo.java
@@ -0,0 +1,12 @@
+public class Foo {
+    
+    //public Foo(){
+    //    
+    //}
+    
+    
+    public static void main(String[] args){
+        Foo fighter =  new Foo();
+        System.out.println(fighter);
+    }
+}
