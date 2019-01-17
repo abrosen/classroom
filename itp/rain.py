3df07a82640af0870f6c1ab6db8560a7a58bdffa
Andrew Rosen <profandrewrosen@gmail.com>
2019-01-17 15:56:23 -0500

    Here

---
+++ itp/rain.py
@@ -0,0 +1,11 @@
+temp = 37
+
+if temp < 40:
+    print("Brrr")
+    print("Better Bring a coat")
+
+    if temp <= 32:
+        print("Because it's literally freezing outside")
+
+    print("What do I look like, a weatherman?")
+print("Done with program")
