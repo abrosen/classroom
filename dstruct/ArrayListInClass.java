d08cd9563c3cb744ff246629ec8ace28fa91ca76
Andrew Rosen <profandrewrosen@gmail.com>
2019-01-29 13:45:08 -0500

    code

---
+++ dstruct/ArrayListInClass.java
@@ -0,0 +1,30 @@
+import java.util.Collections;
+import java.util.List;
+
+public class ArrayListInClass {
+
+
+    // [A,A,B]
+    // [A,A,B]
+    public static <E> boolean isPermutation(List<E> A, List<E> B) {
+        if(A.size() != B.size()) {
+            return false;
+        }
+        for(E item :  A) {
+            int countA = 0;
+            int countB = 0;
+
+            for (int i = 0; i < A.size(); i++) {
+                if(A.get(i).equals(item)){ countA++; }
+            }
+
+            for (int i = 0; i < B.size(); i++) {
+                if(B.get(i).equals(item)) { countB++; }
+            }
+            if (countA != countB) { return false; }
+
+        }x
+        return true;
+    }
+}
+
