6bb2a6100ff5cdbf43727797891c458758c04bf9
Andrew Rosen <profandrewrosen@gmail.com>
2019-04-04 09:19:53 -0400

    Files

---
+++ dstruct/CheatingHangman.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+public class CheatingHangman {
+
+    // read in all words from words.txt
+    // splits them up based on word length
+    // Key is length of word
+    // Value is list of all words that are that length
+    public static HashMap<Integer, List<String>> getInitialWordLists(){
+
+        return null;
+    }
+
+
+    public static HashMap<String, List<String>> generateWordFamilies(HashSet<Character> guessedLetters,
+                                                                     ArrayList<String> wordList){
+
+        return null;
+
+    }
+
+
+    public static List<String> getNewWordList(HashMap<String, List<String>> wordFamilies) {
+        return null;
+    }
+
+
+}
