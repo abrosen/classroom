fa6a4ab6b80f2809f6c15f475fbfc4efe4c5616d
Andrew Rosen <andrew.rosen@temple.edu>
2023-04-04 14:49:21 -0400

    Update to part 6

--- itp/pig.py
+++ itp/pig.py
@@ -35,7 +35,42 @@ def holdAtXOutcomes(trials, limit):
         print(score, outcomes[score]/trials)
 
 
-holdAtXOutcomes(100000, 100)
+def holdAt20orGoal(limit ,score):
+    turnTotal = 0
+    while turnTotal < limit and turnTotal + score < 100 :
+        roll =  random.randint(1,6)
+        #print("Roll:", roll)
+        if roll  == 1:
+            turnTotal = 0
+            return 0, score
+        else:
+            turnTotal += roll
+    return turnTotal, score+turnTotal
+
+def holdAt20orGoalGame():
+    score = 0
+    turns = 0
+    while score < 100:
+        turnTotal, newScore = holdAt20orGoal(18,score)
+        turns += 1
+        #print("Turn Total:", turnTotal)
+        #print("Score:", newScore)
+        score = newScore
+    return turns
+
+def averagePigTurns():
+    totalTurns = 0
+    TRIALS = 100000
+    for _ in range(TRIALS):
+        turns = holdAt20orGoalGame()
+        totalTurns += turns
+    print(totalTurns / TRIALS)
+
+averagePigTurns()
+#holdAt20orGoalGame()
+#turnScore, newScore = holdAt20orGoal(99)
+#print(turnScore, newScore)
+#holdAtXOutcomes(100000, 100)
 #holdAt20Outcomes(100000)
 #x=holdAt20()
 #print(x)
